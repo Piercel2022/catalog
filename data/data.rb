8db888553212cf6437632eef5705e9c689d68c14
Ishpaul Singh <ishpaul207@gmail.com>
2022-10-26 17:15:30 +0530

    added data preservation for book/label

--- data/data.rb
+++ data/data.rb
@@ -1,6 +1,8 @@
 require 'json'
 require_relative '../classes/game'
 require_relative '../classes/author'
+require_relative '../classes/book'
+require_relative '../classes/label'
 require_relative '../classes/source'
 require_relative '../classes/movie'
 require_relative '../classes/music_album'
@@ -9,6 +11,8 @@ class Data
   def initialize
     @games = JSON.parse(File.read('data/game.json'))
     @authors = JSON.parse(File.read('data/author.json'))
+    @books = JSON.parse(File.read('data/books.json'))
+    @labels = JSON.parse(File.read('data/labels.json'))
     @movies = JSON.parse(File.read('data/movies.json'))
     @sources = JSON.parse(File.read('data/sources.json'))
     @albums = JSON.parse(File.read('data/albums.json'))
@@ -265,5 +269,87 @@ class Data
       puts "\n"
     end
   end
+
   # albums----------------------------------------------------------------
+  # books-------------------------------------------------------------
+  def add_label(item)
+    print 'OOPS Catlog# '
+    print 'Label title: '
+    title = gets.chomp
+
+    print 'OOPS Catlog# '
+    print 'Label color: '
+    color = gets.chomp
+
+    label = Label.new(title, color)
+    item.add_label(label)
+  end
+
+  def add_book
+    print 'OOPS Catlog# '
+    print 'Publisher: '
+    publisher = gets.chomp
+
+    print 'OOPS Catlog# '
+    print 'State of the cover (good or bad): '
+    cover_state = gets.chomp
+
+    print 'OOPS Catlog# '
+    print 'Publish date: '
+    date_published = gets.chomp
+    book = Book.new(date_published, publisher, cover_state)
+
+    add_label(book)
+
+    store_book(book)
+  end
+
+  def store_book(book)
+    label_obj = {
+      title: book.label.title,
+      color: book.label.color
+    }
+
+    book_obj = {
+      publisher: book.publisher,
+      cover_state: book.cover_state,
+      date_published: book.date_published.to_s,
+      label: label_obj
+    }
+    # push it to the book json obj
+    @books << book_obj
+    @labels << label_obj
+    # override
+    File.write('data/books.json', JSON.generate(@books))
+    File.write('data/labels.json', JSON.generate(@labels))
+
+    @books = JSON.parse(File.read('data/books.json'))
+    @labels = JSON.parse(File.read('data/labels.json'))
+  end
+
+  def show_books
+    puts "\n"
+    @books.each_with_index do |book, i|
+      print 'OOPS Catlog# '
+      puts "#{i + 1} ->"
+      puts "Publisher: #{book['publisher']} "
+      puts "Date published: #{book['date_published']} "
+      puts "Cover state: #{book['cover_state']} "
+      puts "Title: #{book['label']['title']}"
+      puts "Color: #{book['label']['color']}"
+      puts "\n"
+    end
+  end
+
+  def show_labels
+    puts "\n"
+    @labels.each_with_index do |label, i|
+      print 'OOPS Catlog# '
+      puts "#{i + 1} ->"
+      puts "Title: #{label['title']}"
+      puts "Color: #{label['color']}"
+      puts "\n"
+    end
+  end
+  # books-------------------------------------------------------------
 end

2003ffe730c31f181bd8f4f12f7fd41e2d695f55
29td <nonhlanhlamndebele9@gmail.com>
2022-10-26 10:42:53 +0200

    add data preservation for author class

--- schema.rb
+++ schema.rb
@@ -1,13 +1,13 @@
 CREATE TABLE game (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  multiplayer BOOLEAN,
-  last_played_at DATE,
-  publish_date DATE,
+  multiplayer BOOLEAN
+  last_played_at DATE
+  publish_date DATE
   archived BOOLEAN
 );
 
 CREATE TABLE author (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  first_name VARCHAR(255),
+  first_name VARCHAR(255)
   last_name VARCHAR(255)
 );
